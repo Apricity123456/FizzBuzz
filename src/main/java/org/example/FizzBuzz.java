d590d75fcc03f0d6cd3507946f9b40577384fc43
Shangyu YANG <shangyu.yang@eleve.isep.fr>
2025-11-24 16:20:20 +0100

    new requirements failed

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -10,11 +10,18 @@ public class FizzBuzz {
      * @return placeholder string representation of n
      */
     public static String compute(int n) {
-        boolean fizz = (n % 3 == 0);
-        boolean buzz = (n % 5 == 0);
-        if (fizz && buzz) return "FizzBuzz";
-        if (fizz) return "Fizz";
-        if (buzz) return "Buzz";
-        return Integer.toString(n);
+        boolean mult3 = (n % 3 == 0);
+        boolean mult5 = (n % 5 == 0);
+        String s = Integer.toString(n);
+        boolean contains3 = s.indexOf('3') >= 0;
+        boolean contains5 = s.indexOf('5') >= 0;
+
+        boolean isFizz = mult3 || contains3;
+        boolean isBuzz = mult5 || contains5;
+
+        if (isFizz && isBuzz) return "FizzBuzz";
+        if (isFizz) return "Fizz";
+        if (isBuzz) return "Buzz";
+        return s;
     }
 }
